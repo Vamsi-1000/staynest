dea445acab6d0a2d31c8ae75d89c4348844d63ce
Vamsi-1000 <svamsi100@gmail.com>
2025-07-28 23:13:34 +0530

    Update db.js with new code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,26 @@
 // db.js
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.resolve('config/.env') });
 
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const connection = mysql.createConnection({
+host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD, 
+database: process.env.DB_NAME,
+port: process.env.DB_PORT
+  
 });
 
-console.log('Using DB:', process.env.DB_NAME);
-export default pool;
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err.stack);
+    return;
+  }
+  console.log('✅ Connected to MySQL database.');
+});
+
+export default connection;
 
