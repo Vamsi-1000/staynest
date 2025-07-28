a283e5fa57379d3de05e353158e291db6413e7ec
Vamsi-1000 <svamsi100@gmail.com>
2025-07-29 00:37:27 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,12 +1,10 @@
-const db = require('../config/db');
-const bcrypt = require('bcryptjs');
+import db from '../config/db.js';
+import bcrypt from 'bcryptjs';
 
-const createUser = (name, email, phone, password, callback) => {
+export const createUser = (name, email, phone, password, callback) => {
   bcrypt.hash(password, 10, (err, hashedPassword) => {
     if (err) return callback(err);
     const sql = 'INSERT INTO users (name, email, phone, password) VALUES (?, ?, ?, ?)';
     db.query(sql, [name, email, phone, hashedPassword], callback);
   });
 };
-
-module.exports = { createUser };
